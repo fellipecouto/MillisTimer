097ed5d180ac18bd0895f86f2fa4db7ebd025769
Fellipe Couto <34248343+fellipecouto@users.noreply.github.com>
2020-06-07 13:35:36 -0300

    Add files via upload

--- MillisTimer.cpp
+++ MillisTimer.cpp
@@ -1,7 +1,7 @@
 /*-----------------------------------------------------------------------------------
   Program  : MillisTimer
   Descr    : Library to use the Millis function as a task scheduler.
-             (Be careful with using the Delay function in other parts of the program.
+             (Be careful with using the Delay function in other parts of the program)
   Author   : Fellipe Couto [ http://www.efeitonerd.com.br ]
   GitHub   : https://github.com/fellipecouto/MillisTimer
   Date     : 2020-06-07
